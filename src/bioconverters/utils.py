3da459007f22473d8d165d28428b48d97c614afe
Caralyn Reisle <creisle@bcgsc.ca>
2021-10-26 13:34:59 -0700

    Convert in-text citations to bioc annotations

--- src/bioconverters/utils.py
+++ src/bioconverters/utils.py
@@ -1,7 +1,11 @@
 import re
 import unicodedata
+import uuid
 import xml.etree.cElementTree as etree
-from typing import Callable, Dict, Iterable, List
+from typing import Callable, Dict, Iterable, List, Tuple
+
+import bioc
+from bioc.bioc import BioCAnnotation
 
 # XML elements to ignore the contents of
 IGNORE_LIST = [
@@ -39,16 +43,29 @@ class TextChunk:
     xml_path: str
     non_separating: bool = False
     is_tail: bool = False
+    is_annotation: bool = False
 
-    def __init__(self, text, xml_node, xml_path=None, non_separating=False, is_tail=False):
+    def __init__(
+        self,
+        text,
+        xml_node,
+        xml_path=None,
+        non_separating=False,
+        is_tail=False,
+        is_annotation=False,
+    ):
         self.text = text
         self.xml_node = xml_node
         self.xml_path = xml_path
-        self.non_separating = non_separating
+        self.non_separating = non_separating or is_annotation
         self.is_tail = is_tail
+        self.is_annotation = is_annotation
 
     def __str__(self) -> str:
         return self.text
+
+    def __len__(self) -> int:
+        return len(self.text)
 
 
 TagHandlerFunction = Callable[[etree.Element, Dict[str, Callable]], List[TextChunk]]
@@ -90,7 +107,7 @@ def cleanup_text(text: str) -> str:
     text = re.sub(r'\s+\.(\s|$)', r'.\1', text)
 
     # remove extra spaces around commas
-    text = re.sub(r'\s+,\s+', ', ', text)
+    text = re.sub(r'\s*,\s+', ', ', text)
     return text.strip()
 
 
@@ -129,6 +146,7 @@ def merge_adjacent_xref_siblings(elem_list):
                 siblings[-1].tail
                 and len(prev_tail) == 1
                 and unicodedata.category(prev_tail)[0] == 'P'
+                and elem.attrib.get('ref-type') == siblings[-1].attrib.get('ref-type')
             ):
                 siblings[-1].text = siblings[-1].text + siblings[-1].tail + elem.text
                 siblings[-1].tail = elem.tail
@@ -178,19 +196,71 @@ def tag_handler(
 
     if elem.tag == 'xref' and 'xref' in IGNORE_LIST:
         # keep xref tags that refer to internal elements like tables and figures
-        if not re.search(r'\b(Figure|Fig|Table)s?(\.|\b)', head, re.IGNORECASE):
-            return [TextChunk(tail, elem, is_tail=True)]
+        if elem.attrib.get('ref-type', '') == 'bibr':
+            return [TextChunk(head, elem, is_annotation=True), TextChunk(tail, elem, is_tail=True)]
     elif elem.tag in IGNORE_LIST:
         # Check if the tag should be ignored (so don't use main contents)
-        return [TextChunk(tail, elem, non_separating=True, is_tail=True)]
+        return [
+            TextChunk(tail, elem, non_separating=True, is_tail=True),
+        ]
 
     return [TextChunk(head, elem)] + child_passages + [TextChunk(tail, elem, is_tail=True)]
+
+
+def strip_annotation_markers(
+    text: str, annotations_map: Dict[str, str]
+) -> Tuple[str, List[bioc.BioCAnnotation]]:
+    """
+    Given a set of annotations, remove any which are found in the current text and return
+    the new string as well as the positions of the annotations in the transformed string
+    """
+    matched_annotations: List[Tuple[int, int.str]] = []
+    for ann_marker in annotations_map:
+        # citation in brackets
+        patterns = [
+            (r'\(' + re.escape(ann_marker) + r'\)', 0),  # citation in brackets
+            (
+                r'\s' + re.escape(ann_marker) + r'\.',
+                1,
+            ),  # citation at end of sentence, remove extra whitespace
+            (r'\s' + re.escape(ann_marker) + r'\s', 1),  # citation surrounded by whitespace
+            (re.escape(ann_marker), 0),  # citation by itself
+        ]
+        for pattern, end_offset in patterns:
+            match = re.search(pattern, text)
+            if match:
+                matched_annotations.append((match.start(), match.end() - end_offset, ann_marker))
+                break
+
+    transformed_annotations: List[bioc.BioCAnnotation] = []
+    transformed_text = text
+    offset = 0
+
+    def find_last_token_pos(string):
+        match = re.search(r'(\S+)\s*$', string)
+        return match.start(1), match.end(1)
+
+    for start, end, marker in matched_annotations:
+        ann = bioc.BioCAnnotation()
+        ann.id = marker
+        ann.infons['citation_text'] = annotations_map[marker]
+        ann.infons['type'] = 'citation'
+        token_start, token_end = find_last_token_pos(transformed_text[: start - offset])
+        transformed_text = transformed_text[: start - offset] + transformed_text[end - offset :]
+
+        ann.text = transformed_text[token_start:token_end]
+        ann.add_location(bioc.BioCLocation(token_start, len(ann.text)))
+
+        offset += end - start
+        transformed_annotations.append(ann)
+    return transformed_text, transformed_annotations
 
 
 def extract_text_chunks(
     element_list: Iterable[etree.Element],
     passage_tags=SEPERATION_LIST,
     tag_handlers: Dict[str, TagHandlerFunction] = {},
+    annotations_map: Dict[str, str] = {},
 ) -> List[TextChunk]:
     """
     Extract and beautify text from a series of XML elements
@@ -220,7 +290,12 @@ def extract_text_chunks(
     def merge_text_chunks(chunk_list):
         merge = []
         for i, chunk in enumerate(chunk_list):
-            if chunk.non_separating or (i > 0 and chunk_list[i - 1].non_separating):
+            if chunk.is_annotation:
+                # add to mapping, replace context with UUID
+                id = f'ANN_{uuid.uuid4()}'
+                merge.append(id)
+                annotations_map[id] = cleanup_text(chunk.text)
+            elif not merge or chunk.non_separating or (i > 0 and chunk_list[i - 1].non_separating):
                 merge.append(chunk.text.strip())
             else:
                 merge.extend([' ', chunk.text.strip()])
@@ -231,7 +306,7 @@ def extract_text_chunks(
         text = cleanup_text(text)
         first_non_tail_node = None
         for chunk in chunk_list:
-            if not chunk.is_tail:
+            if not chunk.is_tail and not chunk.is_annotation:
                 first_non_tail_node = chunk.xml_node
                 break
         return TextChunk(text, first_non_tail_node or chunk_list[0].xml_node)
